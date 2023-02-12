805ca5388afbed10f695d8951b8cbb7267abfead
Merlin Thomas <emteeware@gmail.com>
2023-02-12 12:45:04 +0100

    Add logging

--- src/main/kotlin/net/emteeware/emteestockkeeper/ItemController.kt
+++ src/main/kotlin/net/emteeware/emteestockkeeper/ItemController.kt
@@ -1,12 +1,23 @@
 package net.emteeware.emteestockkeeper
 
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
-import java.util.UUID
+import java.util.*
 
 @RestController
 class ItemController(val service: ItemService) {
+
+    var logger: Logger = LoggerFactory.getLogger(ItemController::class.java)
+
     @GetMapping("/item")
-    fun index(): List<Item> = service.findItems()
+    fun index() : List<Item> {
+        logger.warn("GET request")
+        val retVal = service.findItems()
+        logger.warn("Returning $retVal")
+        return retVal
+    }
 
     @GetMapping("/item/{id}")
     fun index(@PathVariable id: Int): List<Item> = service.findItemById(id)
